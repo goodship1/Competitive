125807b8dcb8454fd28847ae74a549332e55f40f
Gavin Goodship <msgoodship1@gmail.com>
2022-05-25 12:01:20 +0100

    Create convertarraytobst.py

---
+++ solutions/convertarraytobst.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        #mid = len(nums) // 2
+        if len(nums) == 0:
+            return None
+        root = TreeNode(nums[len(nums)//2])
+        root.left = self.sortedArrayToBST(nums[:len(nums)//2])
+        root.right = self.sortedArrayToBST(nums[len(nums)//2+1:])
+        return root
